c5a591c115fe42dd5c6e843c0dfb387f266252c2
MaLos <mlosnikov@gmail.com>
2022-05-08 19:10:32 +0300

    add new projects

---
+++ PersonalityTestSwitchh/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PersonalityTestSwitchh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //programm küsib kasutajalt ta lemmikvärvi
+        //punane-romantiline
+        //sinine-töökas
+        //roheline-looduse sõber
+        //muu-ükssarvik
+
+        Console.WriteLine("Siseta oma lemmikvärv:");
+            string userColor = Console.ReadLine();
+
+        switch (userColor)
+         {
+            case "punane":
+            Console.WriteLine("Oled romantiline");
+            break;
+                case "sinine":
+                    Console.WriteLine("Oled töökas");
+                    break;
+                case "roheline":
+                    Console.WriteLine("Oled loodusesõber");
+                    break;
+                default:
+                    Console.WriteLine($"Oled {userColor} ükssarvik");
+                    break;
+                    }
+            Console.WriteLine("Kena päeva!");
+
+
+        }
+}
+}
